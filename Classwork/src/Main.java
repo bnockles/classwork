d54f9dfbf32cc597637300ac28a5e0073fa7341d
bnockles <bnockles@gmail.com>
2016-01-07 07:26:19 -0500

    nothing exciting

--- Classwork/src/Main.java
+++ Classwork/src/Main.java
@@ -7,6 +7,13 @@ public class Main {
 		 * Let's try this from home!
 		 */
 		 //Here's a comment that was added online, via the browser
+		if(isSquare(-16))System.out.println("Square!");
+		else System.out.println(Math.sqrt(-16)+" not square");
+	}
+	
+	public static boolean isSquare(double d){
+		if(Math.sqrt(d)-(int)Math.sqrt(d)==0)return true;
+		else return false;
 	}
 }
 
