e94192dff8a3f765a7b52b15b55ed164565ff52a
bnockles <bnockles@gmail.com>
2016-01-15 05:41:16 -0500

    home message

--- Classwork/src/Main.java
+++ Classwork/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 		System.out.println("Hello git!");
 		//added a comment
 		/**
-		 * Let's try this from home!
+		 * I am trying to see if I can push this to my designated branch
 		 */
 		 //Here's a comment that was added online, via the browser\
 		 //I need to make "Pull" work
