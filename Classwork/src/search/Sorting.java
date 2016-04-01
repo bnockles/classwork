6a227b69d9a98f81a56c57408c11d2833407515e
Student6 <Student6@10.8.33.123>
2016-04-01 12:17:19 -0400

    Jose's Version

--- Classwork/src/search/Sorting.java
+++ Classwork/src/search/Sorting.java
@@ -10,7 +10,7 @@ public class Sorting {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] arrayToSort={133,0,-5,31,12,91,2};
-		selectionSort(arrayToSort);
+		arrayToSort = mergeSort(arrayToSort);
 		System.out.println("Sorted Array: "+Arrays.toString(arrayToSort));
 	}
 	public static void selectionSort(int[] arrayToSort){
@@ -30,5 +30,59 @@ public class Sorting {
 			arrayToSort[minIndex]=placeHolder;
 		}
 	}
+	public static int[] mergeSort(int[] toSort){
+		if(toSort.length==1) return toSort ;
+		int[] firstHalf = Arrays.copyOf(toSort, toSort.length/2);
+		int[] secondHalf = new int[toSort.length-firstHalf.length];
+		for(int i = 0;i<secondHalf.length;i++){
+			secondHalf[i] = toSort[firstHalf.length+i];
+		}
+
+		firstHalf =  mergeSort(firstHalf);
+		secondHalf =  mergeSort(secondHalf);
+		 System.out.println("Sorting the array gave us first Half: " + Arrays.toString(firstHalf));
+		 System.out.println("Sorting the array gave us Second Half: " + Arrays.toString(secondHalf));
+		 System.out.println("Sorting the array gave us Result: " + Arrays.toString(merge(firstHalf,secondHalf)));
+		 return merge(firstHalf,secondHalf);
+    }
+
+    //helper method for mergeSort
+    public static int[] merge(int[] arrA, int[] arrB){
+    	int[] result = new int[arrA.length+arrB.length];
+    	int aIndex = 0;
+    	int bIndex = 0;
+    	int resultIndex = 0;
+    	while(resultIndex<result.length){
+    		if(arrA[aIndex]>arrB[bIndex]){
+    			result[resultIndex] = arrB[bIndex];
+    			resultIndex++;
+    			bIndex++;
+    		}else if(arrA[aIndex]<arrB[bIndex]){
+    			result[resultIndex] = arrA[aIndex];
+    			resultIndex++;
+    			aIndex++;
+    		}else{
+    			result[resultIndex] = arrA[aIndex];
+    			result[resultIndex+1] = arrA[aIndex];
+    			resultIndex+=2;
+    			aIndex++;
+    			bIndex++;
+    		}
+    		while(aIndex<arrA.length&&!(bIndex<arrB.length)){
+    			result[resultIndex] = arrA[aIndex];
+    			aIndex++;
+    			resultIndex++;
+    		}
+    		while(!(aIndex<arrA.length)&&(bIndex<arrB.length)){
+    			result[resultIndex] = arrB[bIndex];
+    			bIndex++;
+    			resultIndex++;
+    		}
+    	}
+//    	 System.out.println("Sorting the array gave us: " + Arrays.toString(arrA));
+//		 System.out.println("Sorting the array gave us: " + Arrays.toString(arrB));
+//		 System.out.println("Sorting the array gave us: " + Arrays.toString(result));
+    	return result;
+    }
 
 }
