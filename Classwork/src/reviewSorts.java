2d1f5c22e9d2ce5a262e486cbad6de78da41a991
cindyxue0 <cxue8506@bths.edu>
2016-04-01 12:52:41 -0400

    Update reviewSorts.java

--- Classwork/src/reviewSorts.java
+++ Classwork/src/reviewSorts.java
@@ -1,4 +1,20 @@
-public static void selectionSort(int[] arr) {
+public class reviewSorts {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+		int[] a = { 5, 6, 7, 4, 3, 2, 1, 8 };
+		selectionSort(a);
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + " ");
+		}
+
+	}
+
+	public static void selectionSort(int[] arr) {
 		for (int i = 0; i < arr.length; i++) {
 			int min = i;
 			for (int j = i + 1; j < arr.length; j++) {
@@ -12,3 +28,4 @@ public static void selectionSort(int[] arr) {
 		}
 
 	}
+}
