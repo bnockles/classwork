b8a588d7945627cff2920b5741bbb95e04265182
Student6 <Student6@10.8.33.135>
2016-04-01 12:02:50 -0400

    Yash's Version

--- Classwork/src/search/Sorting.java
+++ Classwork/src/search/Sorting.java
@@ -1,5 +1,7 @@
 package search;
 
+import java.util.Arrays;
+
 public class Sorting {
 
 	/**
@@ -7,7 +9,26 @@ public class Sorting {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] arrayToSort={133,0,-5,31,12,91,2};
+		selectionSort(arrayToSort);
+		System.out.println("Sorted Array: "+Arrays.toString(arrayToSort));
+	}
+	public static void selectionSort(int[] arrayToSort){
+		int minIndex=0;
+		int minimum=arrayToSort[0];
+		for(int i=0;i<arrayToSort.length;i++){
+			minIndex=i;
+			minimum=arrayToSort[i];
+			for(int j=i;j<arrayToSort.length;j++){
+				if(arrayToSort[j]<minimum){
+					minimum=arrayToSort[j];
+					minIndex=j;
+				}
+			}
+			int placeHolder=arrayToSort[i];
+			arrayToSort[i]=minimum;
+			arrayToSort[minIndex]=placeHolder;
+		}
 	}
 
 }
