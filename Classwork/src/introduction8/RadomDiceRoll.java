da08e63287e21cfe903c4f3d5c333a32d4b05617
bnockles <bnockles@gmail.com>
2016-09-21 14:05:03 -0400

    String methods

--- Classwork/src/introduction8/RadomDiceRoll.java
+++ Classwork/src/introduction8/RadomDiceRoll.java
@@ -6,7 +6,8 @@ public class RadomDiceRoll {
 
 		int[] results = new int[6];
 		//declare variable, logic test, increment
-		for(int index = 0; index < 10000; index ++){	
+		int totalRolls = 10000;
+		for(int index = 0; index < totalRolls; index ++){	
 			int result = rollFairDie();
 			System.out.println("Roll #"+(index+1)+
 					": "+result);
@@ -15,8 +16,11 @@ public class RadomDiceRoll {
 		}
 		//print the results
 		for(int i = 0; i < 6; i++){
-			System.out.println((i+1)+"appeared" +
-		results[i]+" times.");			
+			double percentage = 
+					((int) (1000*(double)results[i]/totalRolls))
+					/10.0;
+			System.out.println((i+1)+" appeared " +
+		percentage+"%.");			
 		}
 	}
 	
