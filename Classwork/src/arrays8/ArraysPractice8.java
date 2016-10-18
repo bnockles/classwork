6fea5801e20203cec7a4e0ec046faec85c990da8
bnockles <bnockles@gmail.com>
2016-10-18 14:04:38 -0400

    Updated method variations

--- Classwork/src/arrays8/ArraysPractice8.java
+++ Classwork/src/arrays8/ArraysPractice8.java
@@ -3,120 +3,164 @@ package arrays8;
 public class ArraysPractice8 {
 
 	static boolean[] boos3;
-	
+
 	public static void main(String[] args) {
-		//how do you time a process?
+		
 		long currentTime = System.currentTimeMillis();
 		
+		//int totalNumbers = 50;
+		int[] fiftyNumbers = new int[50000];
+		//populate(fiftyNumbers);
+		//print(fiftyNumbers);
+		//randomize(fiftyNumbers);
+		//print(fiftyNumbers);
+		rollDice(fiftyNumbers, 4);
+		print(fiftyNumbers);
+		//count each die roll and provide a percentage
+		countResult(fiftyNumbers, 4);
 		
-		int x = 10;
-		x = increase(x);
-		System.out.println(x);
 		
+		
+		
+		long endTime = System.currentTimeMillis();
+		System.out.println("The process took "+(endTime-currentTime) + " ms.");
+		
+	}
+	
+	private static void countResult(int[] m, int numberOfDice) {
+		System.out.println("CountResultMethod");
+		int[] counter = new int[numberOfDice*6];
+		for(int n: m){
+			counter[n-1] = counter[n-1] +1;
+		}
+		for(int i = numberOfDice - 1; i < counter.length; i ++){
+			System.out.println((i+1)+" appeared " + 100*counter[i]/m.length +" %.");
+		}
+		
+	}
+
+	private static void rollDice(int[] n, int numberOfDice) {
+		System.out.println("RollDiceMethod");
+		
+		for(int i = 0; i < n.length; i++){
+			int dice = 0;
+			for(int j = 0; j < numberOfDice; j++){
+				dice = dice + (int) (1 + (Math.random()*6));
+			}
+			n[i] = dice;
+		}	
+		
+	}
+
+	private static void randomize(int[] n) {
+		for(int i = 0; i < n.length; i++){
+			int rand =(int) (1 + (Math.random()*6));
+			n[i] = rand;
+		}
+		
+	}
+	
+	private static void print(int[] n) {
+		for(int i = 0; i < n.length; i++){
+			System.out.println(n[i]);
+		}
+		
+	}
+
+	private static void populate(int[] n) {
+		for(int i = 0; i < n.length; i++){
+			n[i] = i + 1;
+		}
+		
+	}
+	
+	
+
+	
+
+	private void demonstratePassByValue(){
 		String[] someStrings = new String[1000];
 		standardPopulate(someStrings);
 		String s = someStrings[999];
 		makeSpecial(s);
+		someStrings[999] = getASpecialString();
 		print(someStrings);
 		
-		long endTime = System.currentTimeMillis();
-		System.out.println("The process took "
-				+(endTime-currentTime) + " ms." );
+		
+		
+	}
 	
+	private static String getASpecialString() {
+		String s = "SPECIAL STRING";
+		return s;
 	}
 
-	
-	private static int increase(int i){
-		return ++i;
-		///same as i = i+1;
-	}
-	
-	
 	private static void makeSpecial(String s) {
-		s = "THIS STRING IS SPECIAL!";
+		s = "ThIS STRING IS SPECIAL"; 
+		
 	}
 
 	private static void print(String[] s) {
 		for(int i = 0; i < s.length; i++){
 			System.out.println(s[i]);
 		}
+		
 	}
 
 	private static void standardPopulate(String[] s) {
 		for(int i = 0; i < s.length; i++){
-			String string = "String #"+(i+1);
-			s[i] = string;
+			s[i] = "String #"+(i+1);
 		}
+		
 	}
-
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	public static void initializingArraysExample(){
-		//wo different ways to instatiate an array
+	public static void initializeArraysExample(){
 		boolean[] boos1 = new boolean[3];
 		//this can ONLY be done at the declaration
-		//because it sets size AND content:
+		//because it sets size and content
 		boolean[] boos2 = {false, false, false};
-		//this does NOT work:
 		//boos3 = {false, true, true};
-		//this is all that will work:
+		// this is all that will work
 		boos3 = new boolean[3];
 		
-		/**2 ways of iterating through an array:
+		
+		/**2 ways of iterating through an array
 		STANDARD FOR LOOP
-		        - the index is important to keep track of
-		        - you need to customize the order
-		 */   
-//		for(int i = 0; i < boos1.length; i++){
-//			System.out.println(boos1[i]);
-//		}
-		
+				-the index is important to keep track of
+				-you need to customize the order
+		*/
+		for(int i = 0; i < boos1.length; i++){
+			System.out.println(boos1[i]);
+		}
 		/**
-		 * "FOR - EACH" LOOP
-		 *      - the index is not important
-		 *      - you don't need to customize
-		 *      - automatically assigns a "handle"
-		 *      - faster
-		 */
-//		for(boolean b: boos1){
-//			System.out.println(b);
-//		}
+		"FOR - EACH" LOOP
+				-the index is not important
+				-you do not need to customize
+				-automatically assigns a "handle"
+				-faster to write
+		*/
+		for(boolean b: boos1){
+			System.out.println(b);
+		}
 		
-		//OBJECT ARRAYS
-		String[] someStrings2 = {"a","b","c"};
+		
+		
+		// OBJECT ARRAYS
 		String[] someStrings1 = new String[3];
-		//you can do this to instantiate the elements...
 //		someStrings1[0] = "a";
 //		someStrings1[1] = "b";
 //		someStrings1[2] = "c";
-		//..but it is repetitive
 		
-		//let's try a loop to instantiate:
-		for(int i= 0 ; i < someStrings1.length; i++){
-			someStrings1[i] = "a new String";
+		for(int i = 0; i < someStrings1.length; i++){
+			someStrings1[i] = "a new String"; 
 		}
-		//a loop to print it:
+		
 		for(String s: someStrings1){
 			System.out.println(s);
 		}
+		
 	}
-	
+
 }
 
 
