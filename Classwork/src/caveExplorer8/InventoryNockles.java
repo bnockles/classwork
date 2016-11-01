0ced63a6c28952e72a55d4695489f2b9c1514e89
bnockles <bnockles@gmail.com>
2016-11-01 14:04:37 -0400

    Finished Period 8 co-code

--- Classwork/src/caveExplorer8/InventoryNockles.java
+++ Classwork/src/caveExplorer8/InventoryNockles.java
@@ -2,4 +2,23 @@ package caveExplorer8;
 
 public class InventoryNockles {
 
+	private boolean hasMap;
+	private String map;
+	
+	
+	public InventoryNockles() {
+		hasMap = true;
+		updateMap();
+	}
+
+	private void updateMap() {
+		CaveRoomPd8[][] caves = CaveExplorer.caves;
+		//convert these caves to a string representation
+	}
+
+	public String getDescription() {
+		if(hasMap)return map;
+		else return "You have no inventory.";
+	}
+
 }
