9ea99435d62ae55c4bcb177ac7e1529799e846fe
Teacher <Teacher@10.148.46.197>
2016-12-07 11:50:06 -0500

    updated TextLabel

--- Classwork/src/guiTeacher/TextLabel.java
+++ Classwork/src/guiTeacher/TextLabel.java
@@ -1,36 +1,55 @@
 package guiTeacher;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 
 public class TextLabel implements Visible {
 
+	private int x;
+	private int y;
+	private int w;
+	private int h;
+	private String text;
+	private BufferedImage image;
+	
+	public TextLabel(int x, int y, int w, int h, String text){
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.text = text;
+		image = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+		update();
+	}
+	
 	public BufferedImage getImage() {
-		// TODO Auto-generated method stub
-		return null;
+		return image;
 	}
 
 	public int getX() {
-		// TODO Auto-generated method stub
-		return 0;
+		return x;
 	}
 
 	public int getY() {
-		// TODO Auto-generated method stub
-		return 0;
+		return y;
 	}
 
 	public int getWidth() {
-		// TODO Auto-generated method stub
-		return 0;
+		return y;
 	}
 
 	public int getHeight() {
-		// TODO Auto-generated method stub
-		return 0;
+		return h;
 	}
 
 	public void update() {
-		// TODO Auto-generated method stub
+		Graphics2D g = image.createGraphics();
+		g.setRenderingHint( RenderingHints.KEY_TEXT_ANTIALIASING,
+	             RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		g.setColor(Color.black);
+		g.drawString(text, 4, h-5);
 
 	}
 
