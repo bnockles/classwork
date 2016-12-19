20b25bfc08d8938e49a411cb045bcf31850343c3
Nockles <Nockles@10.8.33.99>
2016-12-19 14:04:48 -0500

    Finished assignment 3

--- Classwork/src/guiPractice8/components/Graphic.java
+++ Classwork/src/guiPractice8/components/Graphic.java
@@ -17,11 +17,25 @@ public class Graphic implements Visible {
 	private boolean loadedImages;
 	
 	
+	public Graphic(int x, int y, double scale, String imageLocation){
+		this.x = x;
+		this.y = y;
+		loadedImages = false;
+		loadImages(imageLocation, scale);
+	}
+	
 	public Graphic(int x, int y, String imageLocation){
 		this.x = x;
 		this.y = y;
 		loadedImages = false;
 		loadImages(imageLocation, 0,0);
+	}
+	
+	public Graphic(int x, int y, int w, int h, String imageLocation){
+		this.x = x;
+		this.y = y;
+		loadedImages = false;
+		loadImages(imageLocation, w,h);
 	}
 	
 	private void loadImages(String imageLocation, int w, int h) {
@@ -39,7 +53,20 @@ public class Graphic implements Visible {
 				g.drawImage(icon.getImage(), 0, 0, null);
 						
 			}else{
-				//use custom size (complete on Monday)
+				//use custom size
+				image = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+				Graphics2D g = image.createGraphics();
+				//select coordinates of top 
+				//left rectangle within image
+				//then select width and height to 
+				//display graphic
+				//THEN of teh icon you want to display
+				//select x,y coordinates and width height
+				//this can split an image into parts
+				g.drawImage(icon.getImage(), 0, 0, w,h,0,0,
+						icon.getIconWidth(),icon.getIconHeight(), null);
+				
+				
 			}
 			loadedImages = true;
 		}catch(Exception e){
@@ -49,8 +76,25 @@ public class Graphic implements Visible {
 	}
 
 	private void loadImages(String imageLocation, double scale) {
-		// TODO Auto-generated method stub
-		
+		try{
+			//get the image from file (FULL SIZE)
+			ImageIcon icon = new ImageIcon(imageLocation);
+			
+			int newWidth = (int)(icon.getIconWidth() * scale);
+			int newHeight = (int)(icon.getIconHeight() * scale);
+			image = new BufferedImage(newWidth, newHeight, 
+					BufferedImage.TYPE_INT_ARGB);
+			Graphics2D g = image.createGraphics();
+			g.drawImage(icon.getImage(),0,0,newWidth,newHeight,
+					0,0,icon.getIconWidth(), 
+					icon.getIconHeight(),null); 
+			
+			
+			loadedImages = true;
+		}catch(Exception e){
+			//in case file is not found
+			e.printStackTrace();
+		}
 	}
 
 	public BufferedImage getImage() {
@@ -58,6 +102,8 @@ public class Graphic implements Visible {
 		return image;
 	}
 
+	
+	
 	public int getX() {
 		// TODO Auto-generated method stub
 		return x;
